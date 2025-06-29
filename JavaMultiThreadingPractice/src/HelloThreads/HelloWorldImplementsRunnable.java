c79780bba5dfe2814232ff68ba5c95682132d0f4
Subrahmanyam956 <subrahmanyam956@gmail.com>
2025-06-29 19:44:59 +0530

    running different types of thread implementations and also notes about them

--- JavaMultiThreadingPractice/src/HelloThreads/HelloWorldImplementsRunnable.java
+++ JavaMultiThreadingPractice/src/HelloThreads/HelloWorldImplementsRunnable.java
@@ -9,9 +9,31 @@ public class HelloWorldImplementsRunnable implements Runnable {
         }
     }
 
+
+    public static void referencingTheMethodToRun() {
+        for(int i = 0; i < 5; i++) {
+            System.out.println("Method Referencing Example");
+        }
+    }
+
     public static void main(String[] args) {
         HelloWorldImplementsRunnable helloWorldImplementsRunnable = new HelloWorldImplementsRunnable();
         Thread t1 = new Thread(helloWorldImplementsRunnable);
         t1.start();
+
+        Thread t2 = new Thread(() -> System.out.println("Lamba Expression Thread"));
+        t2.start();
+
+        Thread t3 = new Thread(HelloWorldImplementsRunnable::referencingTheMethodToRun);
+        t3.start();
+
+        try {
+            t1.join();
+            t2.join();
+            t3.join();
+        } catch (InterruptedException interruptedException) {
+            System.out.println("Exception");
+        }
+
     }
 }
